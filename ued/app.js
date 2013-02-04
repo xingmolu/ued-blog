323ee3beb00714884a4ee18ce86c383d0ec0bbcb
艾荣彬 <59174526@qq.com>
2013-02-04 22:25:19 +0800

    session

--- ued/app.js
+++ ued/app.js
@@ -8,10 +8,13 @@ var express = require('express')
   , http = require('http')
   , path = require('path')
   , partials = require('express-partials')
-  , uedConfig = require('./uedConfig').config;
+  , uedConfig = require('./uedConfig').config
+  , parseCookie = require('express/node_modules/connect').utils.parseCookie
+  , MemoryStore = require('express/node_modules/connect/lib/middleware/session/memory');
 
 uedConfig.upload_dir = uedConfig.upload_dir || path.join(__dirname, 'public', 'user_data', 'images');
 var app = express();
+var storeMemory = new MemoryStore({reapInterval:6000*10});
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -25,6 +28,11 @@ app.configure(function(){
     uploadDir:uedConfig.upload_dir
   }));
   app.use(express.methodOverride());
+  app.use(express.cookieParser(uedConfig.session_secret));
+  app.use(express.session({
+          secret:'aiblog',
+          store: storeMemory                
+  }));
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
